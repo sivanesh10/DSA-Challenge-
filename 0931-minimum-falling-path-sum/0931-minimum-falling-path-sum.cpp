c94515097b485a476f2673a44240d04dd19c9611
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-01-09 08:49:08 +0530

    Time: 0 ms (100%), Space: 14.6 MB (80.46%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
-public:
+
+    //Recurrence ...
+
+
+/*public:
     long long f(int i,int j,vector<vector<int>>& arr,int n,int m,vector<vector<long long>> &dp) {
         if(j<0 || j>=m) return 1e18;
         if(i == 0) return arr[0][j];
@@ -8,32 +12,39 @@ public:
         long long ld = arr[i][j] + f(i-1,j-1,arr,n,m,dp);
         long long rd = arr[i][j] + f(i-1,j+1,arr,n,m,dp);
         return dp[i][j] = min(u,min(ld,rd));
-    }
+    }*/
+    
 public:
     int minFallingPathSum(vector<vector<int>>& arr) {
         int n = arr.size();
         int m = arr[0].size();
         long long ans = 1e9;
-        // vector<vector<long long>> dp (n,vector<long long> (m,0));/
-        // for(int j=0;j<=m-1;j++) {
-        //     dp[0][j] = arr[0][j];
-        // }
+        
+        
+        // Tabulation ...
 
-        //Tabulation ...
 
-        // for(int i = 1; i < n; i++) {
-        //     for(int j = 0; j < m; j++) {
-        //         long long u, ld = 1e9, rd = 1e9;
-        //         u = arr[i][j] + dp[i-1][j];
-        //         if(j > 0) ld = arr[i][j] + dp[i-1][j-1];
-        //         if(j < m-1) rd = arr[i][j] + dp[i-1][j+1];
-        //         dp[i][j] = min (u,min(ld,rd));
-        //     }
-        // }
+        /*
+        vector<vector<long long>> dp (n,vector<long long> (m,0));/
+        for(int j=0;j<=m-1;j++) {
+            dp[0][j] = arr[0][j];
+        }
 
-        // for(int i=0;i<m;i++) {
-        //     ans = min(ans,dp[n-1][i]);
-        // }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                long long u, ld = 1e9, rd = 1e9;
+                u = arr[i][j] + dp[i-1][j];
+                if(j > 0) ld = arr[i][j] + dp[i-1][j-1];
+                if(j < m-1) rd = arr[i][j] + dp[i-1][j+1];
+                dp[i][j] = min (u,min(ld,rd));
+            }
+        }
+
+        for(int i=0;i<m;i++) {
+            ans = min(ans,dp[n-1][i]);
+        }
+        */
 
 
         // Space Optimization .. 
