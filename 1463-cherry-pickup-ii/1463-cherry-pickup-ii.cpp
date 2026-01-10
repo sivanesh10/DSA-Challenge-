9366a4d845df6add1d6e3b0f80b2d31ae89a1daa
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-01-10 16:42:00 +0530

    Time: 50 ms (53.27%), Space: 13.3 MB (96.88%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,40 +1,73 @@
 class Solution {
-public:
-    int r(int i,int j1,int j2,vector<vector<int>>& arr,int n,int m,vector<vector<vector<int>>> &dp) {
+// public:
+//     int r(int i,int j1,int j2,vector<vector<int>>& arr,int n,int m,vector<vector<vector<int>>> &dp) {
         
-        //out of bound
-        if(j1 < 0 || j2 < 0 || j1 > m-1 || j2 > m-1) {
-            return -1e9;
-        } 
+//         //out of bound
+//         if(j1 < 0 || j2 < 0 || j1 > m-1 || j2 > m-1) {
+//             return -1e9;
+//         } 
         
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+//         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
 
-        //Base condition
-        if(i == n-1) {
-            if(j1 == j2) {
-                return arr[i][j1]; 
-            }
-            return arr[i][j1] + arr[i][j2];
-        }
+//         //Base condition
+//         if(i == n-1) {
+//             if(j1 == j2) {
+//                 return arr[i][j1]; 
+//             }
+//             return arr[i][j1] + arr[i][j2];
+//         }
 
 
-        int maxi = -1e9;
-        for(auto dj1 : {-1,0,+1}) {
-            for(auto dj2 : {-1,0,+1}) {
-                int cherry = 0;
-                if (j1 != j2) cherry = arr[i][j1] + arr[i][j2];
-                else cherry = arr[i][j1];
-                cherry += r(i+1,j1+dj1,j2+dj2,arr,n,m,dp);
-                maxi =  max(maxi,cherry);
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
+//         int maxi = -1e9;
+//         for(auto dj1 : {-1,0,+1}) {
+//             for(auto dj2 : {-1,0,+1}) {
+//                 int cherry = 0;
+//                 if (j1 != j2) cherry = arr[i][j1] + arr[i][j2];
+//                 else cherry = arr[i][j1];
+//                 cherry += r(i+1,j1+dj1,j2+dj2,arr,n,m,dp);
+//                 maxi =  max(maxi,cherry);
+//             }
+//         }
+//         return dp[i][j1][j2] = maxi;
+//     }
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int>(m,-1)));
-        return r(0,0,m-1,grid,n,m,dp);
+    int cherryPickup(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        // vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int>(m,-1)));
+        vector<vector<int>> prev(m,vector<int>(m,-1e9)), dp(m,vector<int>(m,-1e9));
+        for(int i = 0;i<m;i++) {
+            for(int j = 0;j<m;j++) {
+                if(i == j) {
+                    prev[i][j] = arr[n-1][i];
+                }
+                else prev[i][j] = arr[n-1][i] + arr[n-1][j];
+            }
+        }
+        
+
+        for(int i = n-2;i>=0;i--) {
+            for(int j1 = 0;j1<m;j1++) {
+                for(int j2 = 0;j2<m;j2++) {
+                    int maxi = -1e9;
+                    for(auto dj1 : {-1,0,+1}) {
+                        for(auto dj2 : {-1,0,+1}) {
+                            int cherry = 0;
+                            if (j1 != j2) cherry = arr[i][j1] + arr[i][j2];
+                            else cherry = arr[i][j1];
+                            if(j1+dj1 >= m || j2+dj2 >= m || j1+dj1 < 0 || j2+dj2 < 0) continue;
+                            else {
+                                cherry += prev[j1+dj1][j2+dj2];
+                                maxi =  max(maxi,cherry);
+                            }
+                        }
+                    }
+                    dp[j1][j2] = maxi;                   
+                }
+            }
+            prev = dp;
+        }
+        return prev[0][m-1];
+        // return r(0,0,m-1,grid,n,m,dp);
     }
 };
