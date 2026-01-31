797b92199b22f2970a6245b3f7bc6e6929ce0d5c
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-01-31 08:29:39 +0530

    Time: 47 ms (24.4%), Space: 21.5 MB (21.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -13,7 +13,16 @@ private:
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return f(0,nums,n,dp);
+        vector<int> dp(n,0);
+        dp[n-1] = 0;
+        for (int i = n-2; i >= 0; i--) {
+            int take = 1e9;
+            if(nums[i] == 0) dp[i] = 1e9;
+            for(int j = 1; j <= nums[i]; j++) {
+                if(i+j <= n-1) take = min(1 + dp[i+j],take);
+            }
+            dp[i] = take;
+        }
+        return dp[0];
     }
 };
