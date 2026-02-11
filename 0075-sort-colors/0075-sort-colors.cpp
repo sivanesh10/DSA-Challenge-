cd2a632b887d9d4d6f51ed798228a887eb14da58
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-02-11 19:37:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.31%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int l = 0;
+        int h = arr.size()-1;
+        int m = 0;
+        while(m<=h) {
+            if(arr[m] == 0) {
+                swap(arr[l++],arr[m++]);
+            }
+            else if(arr[m] == 1) {
+                m++;
+            }
+            else {
+                swap(arr[m],arr[h--]);
+            }
+        }
+    }
+};
