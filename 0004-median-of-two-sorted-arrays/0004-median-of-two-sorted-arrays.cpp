9cd0b6b6694e496289e4c205631e7a0a922cd500
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-03-06 13:25:50 +0530

    Time: 3 ms (29.66%), Space: 95.7 MB (33.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,4 +1,38 @@
 class Solution {
+public :
+    double better(vector<int>& nums1, vector<int>& nums2,int n,int m) {
+        vector<int> arr(n+m);
+        int l = 0;
+        int r = 0;
+        int id = 0;
+        while(l < n && r < m) {
+            if(nums1[l] < nums2[r]) {
+                arr[id] = nums1[l++];
+            }
+            else if(nums1[l] > nums2[r]) {
+                arr[id] = nums2[r++];
+            }
+            else {
+                arr[id] = nums1[l++];
+            }
+            id++;
+        }
+        while (l < n) {
+            arr[id++] = nums1[l++];
+        }
+        while (r < m) {
+            arr[id++] = nums2[r++];
+        }
+        if((m+n)%2 != 0) {
+            return arr[(m+n-1)/2];
+        }
+        else {
+            int idx = (n+m-1)/2;
+            double nu = (arr[idx] + arr[idx+1]) / 2.0;
+            return nu;
+        }
+        return -1;
+    }
 public: 
     double brute(vector<int>& nums1, vector<int>& nums2,int n,int m) {
         vector<int> arr(n+m);
