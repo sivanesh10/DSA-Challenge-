ee102951f1dfe865a8a325c74701b9bc5f3f1832
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-06-05 07:38:54 +0530

    Time: 532 ms (4.9%), Space: 71.8 MB (14.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,16 +4,20 @@ public:
         int n = nums.size();
         int sum = 0;
         int maxi = INT_MIN;
+        int start = 0;
+        int end;
 
         for(int i=0;i<n;i++) {
             sum += nums[i];
             
             if(sum > maxi) {
                 maxi = sum;
+                end = i;
             }
 
-            if(sum < 0) sum = 0; 
+            if(sum < 0) { sum = 0; start = i+1;} 
         }
+        for(int it = start;it <= end;it++) cout << nums[it] << "  ";
         return maxi;
     }
 };
