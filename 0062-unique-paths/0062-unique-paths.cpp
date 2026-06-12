ac7bf06262076076d64188bda8b06fcc5d02c97b
Sivanesh_Ravichelvam <sivaneshravichelvam@gmail.com>
2026-06-12 08:46:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (75.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,8 +13,27 @@ public:
             r = dfs(i-1,j,m,n,dp);//down
         return dp[i][j] = l + r;
     }
+    int tabu(int m, int n,vector<int>& prev,vector<int>& curr) {
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i == 0 && j == 0) curr[j] = 1;
+                else {
+                    int above = 0;
+                    int left = 0;
+                    if(i > 0) above = prev[j];
+                    if(j > 0) left = curr[j-1];
+                    curr[j] = above + left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return dfs(m-1,n-1,m,n,dp);
+        // vector<vector<int>> dp(m,vector<int> (n,0));
+        vector<int> prev(n,0);
+        vector<int> curr(n,0); 
+        // return dfs(m-1,n-1,m,n,dp);
+        return tabu(m,n,prev,curr);
     }
 };
